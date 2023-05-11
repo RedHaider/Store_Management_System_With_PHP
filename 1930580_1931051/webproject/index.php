99a36f0fc370c91d7a2140003893ab00f3ef9292
RedHaider <132941612+RedHaider@users.noreply.github.com>
2023-05-11 11:23:37 +0600

    Add files via upload

---
+++ 1930580_1931051/webproject/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<link rel="stylesheet" type="text/css" href="style1.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>welcome Page</title>
+</head>
+<body>
+
+	<!--navbar-->
+
+	<nav class="navbar navbar-expand bg-dark navbar-dark py-3 " >
+	<div class="container">
+		<a href="index.php" class="navbar-brand">VOGUE LIFESTYLE</a>
+		<ul class="navbar-nav  ms-auto">
+    	  	<li class="nav-item">
+    	  		<a href="signup.php" class="nav-link">Sign Up</a>
+    	  	</li>
+    	  	<li class="nav-item">
+    	  		<a href="user.php" class="nav-link">Login</a>
+    	  	</li>
+    	  	<li class="nav-item">
+    	  		<a href="admin.php" class="nav-link">Admin</a>
+    	  	</li>
+    	  </ul>     
+  </div>
+	</div>
+</nav>
+<!--Card middle body-->
+<section class="p-5" >
+	<div class="container">
+		<div class="row text-center g-4 ">
+			<div class="col-md ">
+				
+				      
+				      <h3 class="card-title mb-3 mt-3">WELCOME TO VOGUES LIFESTYLE</h3>
+				      <p class="card-text">
+				      	Create your account today!
+				      </p>
+				      <p class="card-text">
+				      	Explore us to find your perfect dress
+				      </p  >
+				      <div class="d-flex justify-content-start flex-wrap">
+				      <img class="rounded m-1 border border-dark" width="700" height="400" src="font_image/index1.jpg" alt="Card image cap">
+				      <img class="rounded m-1 border border-dark" width="400" height="400" src="font_image/index2.jpg"  alt="Card image cap">
+				      <img class="rounded m-1 border border-dark" width="400" height="400" src="font_image/index3.jpg" alt="Card image cap">
+				      <img class="rounded m-1 border border-dark" width="400" height="400" src="font_image/index4.jpg" alt="Card image cap">
+				      <img class="rounded m-1 border border-dark" width="400" height="400" src="font_image/index5.jpg" alt="Card image cap">
+			        </div>
+				      
+					  
+			   </div>		
+		   </div>
+	   </div>
+   </section>
+<!--footer-->
+<div class="footer ">
+	<footer class=" p-3  bg-dark text-white text-center ">
+ <div class="container">
+  <p class="lead">Our Social Media Contacts</p>
+  <a href="#"><i class="bi bi-twitter text-light mx-1"></i></a>
+    <a href="#"><i class="bi bi-facebook text-light mx-1"></i></a>
+    <a href="#"><i class="bi bi-linkedin  text-light mx-1"></i></a>
+    <a href="#"><i class="bi bi-instagram text-light mx-1"></i></a>
+ </div> 
+</footer> 
+</div>	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
